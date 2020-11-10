959266fb2a3740fde694350e7e2ee71132468f49
Adda0 <chocholaty.david0@gmail.com>
2020-11-10 19:01:56 +0100

    Don't allocate space for error message in stderr_message

--- src/stderr_message.c
+++ src/stderr_message.c
@@ -18,23 +18,17 @@
 void stderr_message(const char *module, MessageType message_type, CompilerResult compiler_result_arg, const char *fmt, ...) {
     set_compiler_result(compiler_result_arg);
 
-    // Allocate string for formatted message as a string containing string fmt, string module and additional characters
-    // of maximal length of 14.
-    char *format = (char *) malloc(sizeof(char) * (strlen(fmt) + strlen(module) + 14));
-    if (format == NULL) {
-        fprintf(stderr, "stderr_message: error: Malloc of message to stderr failed.\n");
-        set_compiler_result(COMPILER_RESULT_ERROR_INTERNAL);
-        return;
+    fprintf(stderr, "%s: ", module);
+    if (message_type == ERROR) {
+        fprintf(stderr, "error: ");
+    } else {
+        fprintf(stderr, "warning: ");
     }
-
-    sprintf(format, "%s: %s: %s\n", module, (message_type == ERROR) ? "error" : "warning", fmt);
 
     va_list arguments;
     va_start(arguments, fmt);
-    vfprintf(stderr, format, arguments);
+    vfprintf(stderr, fmt, arguments);
     va_end(arguments);
-
-    free(format);
 }
 
 void set_compiler_result(CompilerResult compiler_result_arg) {
