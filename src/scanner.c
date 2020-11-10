be0566481a07b5b20a450a7cbca909765dbb8e6d
Adda0 <chocholaty.david0@gmail.com>
2020-11-10 19:43:49 +0100

    Implement function to get next char from the source code

--- src/scanner.c
+++ src/scanner.c
@@ -34,5 +34,32 @@ ScannerResult scanner_get_token(Token *token, EolRule eol_rule) {
 
     AutomatonState automaton_state = STATE_DEFAULT;
     ScannerResult scanner_result = SCANNER_RESULT_SUCCESS;
+        if (read_char == EMPTY_CHAR) { // get new character from source code
+            next_char_result = get_next_char(&read_char);
+
+            if (next_char_result == NEXT_CHAR_RESULT_ERROR) {
+                stderr_message("scanner", ERROR, COMPILER_RESULT_ERROR_LEXICAL, "Reading a next character from the source code failed.");
+                mstr_free(&mutable_string);
+                return SCANNER_RESULT_INTERNAL_ERROR;
+            }
+
+            if (next_char_result == NEXT_CHAR_RESULT_EOF) {
+                scanner_result = SCANNER_RESULT_EOF;
+            }
+        }
     return scanner_result;
 }
+
+static NextCharResult get_next_char(char *read_char) {
+    *read_char = (char) getchar();
+
+    if (ferror(stdin)) {
+        return NEXT_CHAR_RESULT_ERROR;
+    }
+    if (feof(stdin)) {
+        return NEXT_CHAR_RESULT_EOF;
+    }
+
+    return NEXT_CHAR_RESULT_SUCCESS;
+}
+
