533c984136cd48b6879bf7822939c8de160a35f4
David Chocholatý <chocholaty.david0@gmail.com>
2020-11-11 22:59:53 +0100

    Create template for parser-scanner tests

---
+++ src/tests/parser_scanner.cpp
@@ -0,0 +1,129 @@
+/** @file parser_scanner.cpp
+ *
+ * IFJ20 compiler tests
+ *
+ * @brief Contains tests for the parser and scanner modules.
+ *
+ * @author David Chocholatý (xchoch08), FIT BUT
+ */
+
+// Verbosity levels:
+// 0 – no debug info
+// 1 – info about processed tokens
+// 2 – all info excluding character reads and buffer clearing
+// 3 – all info
+#define VERBOSE 3
+
+#include <iostream>
+#include <array>
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "scanner.h"
+#include "parser.h"
+#include "mutable_string.h"
+
+CompilerResult compiler_result = COMPILER_RESULT_SUCCESS;
+
+int get_char_internal(int *feof, int *ferror) {
+    int c = std::cin.get();
+
+    if (c == EOF) {
+        *feof = 1;
+#if VERBOSE > 2
+        std::cout << "[READ EOF]\n";
+#endif
+        return EOF;
+    }
+
+#if VERBOSE > 2
+    std::cout << "[READ] " << (char) c << '\n';
+    std::cout.flush();
+#endif
+
+    return c;
+}
+}
+
+
+
+class ParserScannerTest : public ::testing::Test {
+protected:
+    std::streambuf *cinBackup;
+    std::stringbuf *buffer;
+
+    void SetUp() override {
+#if VERBOSE > 1
+        std::cout << "[TEST SetUp]\n";
+#endif
+        cinBackup = std::cin.rdbuf();
+
+        buffer = new std::stringbuf();
+        std::cin.rdbuf(buffer);
+    }
+
+    void TearDown() override {
+#if VERBOSE > 1
+        std::cout << "[TEST TearDown]\n";
+#endif
+        int toClear = buffer->in_avail();
+        buffer->pubseekoff(toClear, std::ios_base::cur, std::ios_base::out);
+
+        Token tmp;
+        for (int i = 0; i < 4; i++) {
+#if VERBOSE > 2
+            std::cout << "[CLEARING] " << i << '\n';
+#endif
+            scanner_get_token(&tmp, EOL_OPTIONAL);
+        }
+
+        std::cin.rdbuf(cinBackup);
+        delete buffer;
+    }
+
+    void ComplexTest(std::string &inputStr, CompilerResult expected_compiler_result);
+};
+
+void ParserScannerTest::ComplexTest(std::string &inputStr, CompilerResult expected_compiler_result) {
+#if VERBOSE
+    std::cout << "[TEST] Input string:\n" << inputStr << "\n[START]\n";
+#endif
+    buffer->sputn(inputStr.c_str(), inputStr.length());
+    buffer->sputc(EOF);
+
+    ASSERT_EQ(parser_parse(), expected_compiler_result);
+}
+
+
+
+
+TEST_F(ParserScannerTest, BasicCode) {
+    std::string inputStr = \
+        "package main\n"
+        "\n"
+        "func main() {\n"
+        "    a := 0\n"
+        "    a -= 5\n"
+        "\n"
+        "    if 1 {\n"
+        "        print(\"Will always be printed.\\n\")\n"
+        "    } else {\n"
+        "        print(\"Will never be seen.\n\")\n"
+        "    }\n"
+        "    \n"
+        "    return\n"
+        "}\n";
+
+    ComplexTest(inputStr, COMPILER_RESULT_SUCCESS);
+}
+
+TEST_F(ParserScannerTest, MinimalisticFunction) {
+    std::string inputStr = \
+        "package main\n"
+        "\n"
+        "func main() {\n"
+        "    a := 0\n"
+        "}\n";
+
+    ComplexTest(inputStr, COMPILER_RESULT_SUCCESS);
+}
