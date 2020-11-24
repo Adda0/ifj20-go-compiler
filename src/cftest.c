dd310d5cc4d1059b4285bdc25ae4e0a130f67430
Ondřej Ondryáš <ondryaso@ondryaso.eu>
2020-11-24 13:42:21 +0100

    Extend the CFG testing program

--- src/cftest.c
+++ src/cftest.c
@@ -3,28 +3,65 @@
 #include "symtable.h"
 #include "compiler.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 CompilerResult compiler_result = COMPILER_RESULT_SUCCESS;
 
 int main() {
-    STSymbol i = {.identifier = "i",
-            .type = ST_SYMBOL_VAR,
-            .data = (STSymbolData) {.var_data = (STVariableData) {.type = CF_INT}}};
-    STSymbol a = {.identifier = "a",
-            .type = ST_SYMBOL_VAR,
-            .data = (STSymbolData) {.var_data = (STVariableData) {.type = CF_INT}}};
-    STSymbol b = {.identifier = "b",
-            .type = ST_SYMBOL_VAR,
-            .data = (STSymbolData) {.var_data = (STVariableData) {.type = CF_INT}}};
-    STSymbol fun1 = {.identifier = "fun1",
-            .type = ST_SYMBOL_FUNC,
-            .data = (STSymbolData) {.func_data = (STFunctionData) {.defined=true}}};
-    STSymbol print = {.identifier = "print",
-            .type = ST_SYMBOL_FUNC,
-            .data = (STSymbolData) {.func_data = (STFunctionData) {.defined=true}}};
-    STSymbol aFor = {.identifier = "a",
-            .type = ST_SYMBOL_VAR,
-            .data = (STSymbolData) {.var_data = (STVariableData) {.type = CF_INT}}};
+    SymbolTable *globSt = symtable_init(32);
+    SymbolTable *mainSt = symtable_init(32);
+    SymbolTable *funSt = symtable_init(32);
+    SymbolTable *fun2St = symtable_init(32);
+    SymbolTable *ifThenSt = symtable_init(32);
+    SymbolTable *ifElseSt = symtable_init(32);
+    SymbolTable *forBodySt = symtable_init(32);
+
+    STItem *globMainIt = symtable_add(globSt, "main", ST_SYMBOL_FUNC);
+    globMainIt->data.reference_counter = 2;
+
+    STItem *globFunIt = symtable_add(globSt, "fun", ST_SYMBOL_FUNC);
+    globFunIt->data.reference_counter = 2;
+
+    STItem *globFun2It = symtable_add(globSt, "fun2", ST_SYMBOL_FUNC);
+    globFun2It->data.reference_counter = 2;
+
+    STItem *globPrintIt = symtable_add(globSt, "print", ST_SYMBOL_FUNC);
+    globPrintIt->data.reference_counter = 2;
+
+    STParam *a = calloc(2, sizeof(STParam));
+    a->type = CF_INT;
+    a->id = "a";
+    a->next = (a + 1);
+    a->next->type = CF_INT;
+    a->next->id = "b";
+    globFunIt->data.data.func_data = (STFunctionData) {.defined = true, .params = NULL, .ret_types = a};
+
+    STParam *b = calloc(2, sizeof(STParam));
+    b->type = CF_INT;
+    b->id = NULL; // empty return val name
+    (b + 1)->type = CF_INT;
+    (b + 1)->id = "x";
+    globFunIt->data.data.func_data = (STFunctionData) {.defined = true, .params = (b + 1), .ret_types = b};
+
+    STItem *funIIt = symtable_add(funSt, "i", ST_SYMBOL_VAR);
+    STSymbol *funI = &funIIt->data;
+    funI->data.var_data.type = CF_INT;
+
+    STItem *funAIt = symtable_add(funSt, "a", ST_SYMBOL_VAR);
+    STSymbol *funA = &funAIt->data;
+    funA->data.var_data.type = CF_INT;
+
+    STItem *funBIt = symtable_add(funSt, "b", ST_SYMBOL_VAR);
+    STSymbol *funB = &funBIt->data;
+    funB->data.var_data.type = CF_INT;
+
+    STItem *forAIt = symtable_add(forBodySt, "a", ST_SYMBOL_VAR);
+    STSymbol *forA = &forAIt->data;
+    forA->data.var_data.type = CF_INT;
+
+    STItem *fun2XIt = symtable_add(fun2St, "x", ST_SYMBOL_VAR);
+    STSymbol *fun2X = &fun2XIt->data;
+    fun2X->data.var_data.type = CF_INT;
 
     cf_init();
 
@@ -33,34 +70,46 @@ int main() {
         return 1;
     }
 
-    // func main()
-    cf_make_function("main");
-    cf_add_return_value(NULL, CF_INT);
-    cf_add_return_value(NULL, CF_INT);
+    cf_assign_global_symtable(globSt);
+    // func fun2(int x) int { return x * 2 }
+    cf_make_function("fun2");
+    cf_add_argument("x", CF_INT);
+    cf_assign_function_symtable(fun2St);
+
+    cf_make_next_statement(CF_RETURN);
+    cf_ast_init_for_list(AST_MULTIPLY, 0);
+    cf_ast_add_leaf(AST_LEFT_OPERAND, AST_ID, (ASTNodeData) {.symbolTableItemPtr = fun2X});
+    cf_ast_add_leaf(AST_RIGHT_OPERAND, AST_CONST_INT, (ASTNodeData) {.intConstantValue = 2});
+
+    // func fun() (int, int)
+    cf_make_function("fun");
+    cf_assign_function_symtable(funSt);
 
     // i := 10
     cf_make_next_statement(CF_BASIC);
     cf_ast_init(AST_ROOT, AST_DEFINE);
     cf_use_ast(CF_STATEMENT_BODY);
 
-    cf_ast_add_leaf(AST_LEFT_OPERAND, AST_ID, (ASTNodeData) {.symbolTableItemPtr = &i});
+    cf_ast_add_leaf(AST_LEFT_OPERAND, AST_ID, (ASTNodeData) {.symbolTableItemPtr = funI});
     cf_ast_add_leaf(AST_RIGHT_OPERAND, AST_CONST_INT, (ASTNodeData) {.intConstantValue = 10});
 
-    // i = a * b
+    // i = a * fun2(a)
     cf_make_next_statement(CF_BASIC);
     cf_ast_init(AST_ROOT, AST_ASSIGN);
     cf_use_ast(CF_STATEMENT_BODY);
 
-    cf_ast_add_leaf(AST_LEFT_OPERAND, AST_ID, (ASTNodeData) {.symbolTableItemPtr = &i});
+    cf_ast_add_leaf(AST_LEFT_OPERAND, AST_ID, (ASTNodeData) {.symbolTableItemPtr = funI});
     cf_ast_init(AST_RIGHT_OPERAND, AST_MULTIPLY);
-    cf_ast_add_leaf(AST_LEFT_OPERAND, AST_ID, (ASTNodeData) {.symbolTableItemPtr = &a});
+    cf_ast_add_leaf(AST_LEFT_OPERAND, AST_ID, (ASTNodeData) {.symbolTableItemPtr = funA});
     cf_ast_init(AST_RIGHT_OPERAND, AST_FUNC_CALL);
-    cf_ast_add_leaf(AST_UNARY_OPERAND, AST_ID, (ASTNodeData) {.symbolTableItemPtr = &fun1});
+    cf_ast_add_leaf(AST_LEFT_OPERAND, AST_ID, (ASTNodeData) {.symbolTableItemPtr = &globFun2It->data});
+    cf_ast_init_with_data(AST_RIGHT_OPERAND, AST_LIST, 1);
+    cf_ast_add_leaf_for_list(AST_ID, (ASTNodeData) {.symbolTableItemPtr = funA}, 0);
 
-    // if 5 < 10
+    // if 5 > 10
     cf_make_next_statement(CF_IF);
     // if conditional
-    cf_ast_init(AST_ROOT, AST_LOG_LT);
+    cf_ast_init(AST_ROOT, AST_LOG_GT);
     cf_use_ast(CF_IF_CONDITIONAL);
 
     cf_ast_add_leaf(AST_LEFT_OPERAND, AST_CONST_INT, (ASTNodeData) {.intConstantValue = 5});
@@ -68,34 +117,37 @@ int main() {
 
     // then { fun1() }
     cf_make_if_then_statement(CF_BASIC);
+    cf_assign_statement_symtable(ifThenSt);
     cf_ast_init(AST_ROOT, AST_FUNC_CALL);
     cf_use_ast(CF_STATEMENT_BODY);
 
-    cf_ast_add_leaf(AST_UNARY_OPERAND, AST_ID, (ASTNodeData) {.symbolTableItemPtr = &fun1});
+    cf_ast_add_leaf(AST_UNARY_OPERAND, AST_ID, (ASTNodeData) {.symbolTableItemPtr = &globFunIt->data});
 
     //else { return 1, b - 2 }
     cf_pop_previous_branched_statement();
     cf_make_if_else_statement(CF_RETURN);
+    cf_assign_statement_symtable(ifElseSt);
     // AST (AST_LIST) has been created automatically
     cf_ast_add_leaf_for_list(AST_CONST_INT, (ASTNodeData) {.intConstantValue = 1}, 0);
     cf_ast_init_for_list(AST_SUBTRACT, 1);
-    cf_ast_add_leaf(AST_LEFT_OPERAND, AST_ID, (ASTNodeData) {.symbolTableItemPtr = &b});
+    cf_ast_add_leaf(AST_LEFT_OPERAND, AST_ID, (ASTNodeData) {.symbolTableItemPtr = funB});
     cf_ast_add_leaf(AST_RIGHT_OPERAND, AST_CONST_INT, (ASTNodeData) {.intConstantValue = 2});
 
     cf_pop_previous_branched_statement();
 
     cf_make_next_statement(CF_FOR);
+    cf_assign_statement_symtable(forBodySt);
 
     // for definition (a := 0)
     cf_ast_init(AST_ROOT, AST_DEFINE);
     cf_use_ast(CF_FOR_DEFINITION);
 
-    cf_ast_add_leaf(AST_LEFT_OPERAND, AST_ID, (ASTNodeData) {.symbolTableItemPtr = &aFor});
+    cf_ast_add_leaf(AST_LEFT_OPERAND, AST_ID, (ASTNodeData) {.symbolTableItemPtr = forA});
     cf_ast_add_leaf(AST_RIGHT_OPERAND, AST_CONST_INT, (ASTNodeData) {.intConstantValue = 0});
 
     // for conditional (a < 10)
     cf_ast_init(AST_ROOT, AST_LOG_LT);
-    cf_ast_add_leaf(AST_LEFT_OPERAND, AST_ID, (ASTNodeData) {.symbolTableItemPtr = &aFor});
+    cf_ast_add_leaf(AST_LEFT_OPERAND, AST_ID, (ASTNodeData) {.symbolTableItemPtr = forA});
     cf_ast_add_leaf(AST_RIGHT_OPERAND, AST_CONST_INT, (ASTNodeData) {.intConstantValue = 10});
     cf_use_ast(CF_FOR_CONDITIONAL);
 
@@ -103,9 +155,9 @@ int main() {
     cf_ast_init(AST_ROOT, AST_ASSIGN);
     cf_use_ast(CF_FOR_AFTERTHOUGHT);
 
-    cf_ast_add_leaf(AST_LEFT_OPERAND, AST_ID, (ASTNodeData) {.symbolTableItemPtr = &aFor});
+    cf_ast_add_leaf(AST_LEFT_OPERAND, AST_ID, (ASTNodeData) {.symbolTableItemPtr = forA});
     cf_ast_init(AST_RIGHT_OPERAND, AST_ADD);
-    cf_ast_add_leaf(AST_LEFT_OPERAND, AST_ID, (ASTNodeData) {.symbolTableItemPtr = &aFor});
+    cf_ast_add_leaf(AST_LEFT_OPERAND, AST_ID, (ASTNodeData) {.symbolTableItemPtr = forA});
     cf_ast_add_leaf(AST_RIGHT_OPERAND, AST_CONST_INT, (ASTNodeData) {.intConstantValue = 1});
 
     // for body
@@ -114,18 +166,25 @@ int main() {
     cf_ast_init(AST_ROOT, AST_FUNC_CALL);
     cf_use_ast(CF_STATEMENT_BODY);
 
-    cf_ast_add_leaf(AST_LEFT_OPERAND, AST_ID, (ASTNodeData) {.symbolTableItemPtr = &print});
+    cf_ast_add_leaf(AST_LEFT_OPERAND, AST_ID, (ASTNodeData) {.symbolTableItemPtr = &globPrintIt->data});
     cf_ast_init_with_data(AST_RIGHT_OPERAND, AST_LIST, 1);
-    cf_ast_add_leaf_for_list(AST_ID, (ASTNodeData) {.symbolTableItemPtr = &aFor}, 0);
+    cf_ast_add_leaf_for_list(AST_ID, (ASTNodeData) {.symbolTableItemPtr = forA}, 0);
 
     cf_pop_previous_branched_statement();
 
-    // return i
+    // return i, 0
     cf_make_next_statement(CF_RETURN);
-    cf_ast_add_leaf_for_list(AST_ID, (ASTNodeData) {.symbolTableItemPtr = &i}, 0);
-    cf_ast_add_leaf_for_list(AST_CONST_INT, (ASTNodeData) {.intConstantValue = 128}, 1);
+    cf_ast_add_leaf_for_list(AST_ID, (ASTNodeData) {.symbolTableItemPtr = funI}, 0);
+    cf_ast_add_leaf_for_list(AST_CONST_INT, (ASTNodeData) {.intConstantValue = 0}, 1);
+
+    cf_make_function("main");
+    cf_assign_function_symtable(mainSt);
+    cf_make_next_statement(CF_BASIC);
+    cf_ast_init(AST_ROOT, AST_FUNC_CALL);
+    cf_use_ast(CF_STATEMENT_BODY);
+    cf_ast_add_leaf(AST_UNARY_OPERAND, AST_ID, (ASTNodeData) {.symbolTableItemPtr = &globFunIt->data});
 
     tcg_generate();
 
-    return 0;
+    return compiler_result;
 }
