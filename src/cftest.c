dbb95eb060bfa9082d70ddbdd769f89d71ea101f
Ondřej Ondryáš <ondryaso@ondryaso.eu>
2020-11-27 18:52:14 +0100

    Add recursive factorial test

--- src/cftest.c
+++ src/cftest.c
@@ -400,8 +400,227 @@ void test2() {
     cf_ast_add_leaf(AST_UNARY_OPERAND, AST_ID, (ASTNodeData) {.symbolTableItemPtr = &globFunIt->data});
 }
 
+void factorial_rec() {
+    SymbolTable *glob = symtable_init(32),
+            *factorial = symtable_init(32),
+            *factorial_if_then = symtable_init(32),
+            *factorial_if_else = symtable_init(32),
+            *main = symtable_init(32),
+            *main_if_then = symtable_init(32),
+            *main_if_else = symtable_init(32),
+            *main_if_then_if_then = symtable_init(32),
+            *main_if_then_if_else = symtable_init(32);
+
+    // internal: print
+    STItem *si_print = symtable_add(glob, "print", ST_SYMBOL_FUNC);
+    STSymbol *sym_print = &si_print->data;
+    sym_print->reference_counter = 4;
+    sym_print->data.func_data.defined = false;
+
+    // internal: inputi
+    STItem *si_inputi = symtable_add(glob, "inputi", ST_SYMBOL_FUNC);
+    STSymbol *sym_inputi = &si_inputi->data;
+    sym_inputi->reference_counter = 1;
+    sym_inputi->data.func_data.defined = true;
+    symtable_add_ret_type(si_inputi, NULL, CF_INT);
+    symtable_add_ret_type(si_inputi, NULL, CF_INT);
+
+    // func factorial(n int) (int)
+    STItem *si_factorial = symtable_add(glob, "factorial", ST_SYMBOL_FUNC);
+    STSymbol *sym_factorial = &si_factorial->data;
+    sym_factorial->reference_counter = 2;
+    sym_factorial->data.func_data.defined = true;
+    symtable_add_param(si_factorial, "n", CF_INT);
+    symtable_add_ret_type(si_factorial, NULL, CF_INT);
+
+    STSymbol *sym_factorial_n = &symtable_add(factorial, "n", ST_SYMBOL_VAR)->data;
+    sym_factorial_n->data.var_data = (STVariableData) {.type = CF_INT, .is_return_val_variable = false, .is_argument_variable = true};
+    sym_factorial_n->reference_counter = 3;
+
+    STSymbol *sym_factorial_decN = &symtable_add(factorial, "dec_n", ST_SYMBOL_VAR)->data;
+    sym_factorial_decN->data.var_data = (STVariableData) {.type = CF_INT, .is_return_val_variable = false, .is_argument_variable = false};
+    sym_factorial_decN->reference_counter = 2;
+
+    // func main()
+    STItem *si_main = symtable_add(glob, "main", ST_SYMBOL_FUNC);
+    STSymbol *sym_main = &si_main->data;
+    sym_main->reference_counter = 1;
+    sym_main->data.func_data.defined = true;
+
+    STSymbol *sym_main_a = &symtable_add(main, "a", ST_SYMBOL_VAR)->data;
+    sym_main_a->data.var_data = (STVariableData) {.type = CF_INT, .is_return_val_variable = false, .is_argument_variable = false};
+    sym_main_a->reference_counter = 4;
+
+    STSymbol *sym_main_err = &symtable_add(main, "err", ST_SYMBOL_VAR)->data;
+    sym_main_err->data.var_data = (STVariableData) {.type = CF_INT, .is_return_val_variable = false, .is_argument_variable = false};
+    sym_main_err->reference_counter = 2;
+
+    STSymbol *sym_main_if_then_if_else_vysl = &symtable_add(main_if_then_if_else, "vysl", ST_SYMBOL_VAR)->data;
+    sym_main_if_then_if_else_vysl->data.var_data = (STVariableData) {.type = CF_INT, .is_return_val_variable = false, .is_argument_variable = false};
+    sym_main_if_then_if_else_vysl->reference_counter = 2;
+
+    cf_init();
+    cf_assign_global_symtable(glob);
+
+#define link() do { n->left = n1; n->right = n2; if (!ast_infer_node_type(n)) { fprintf(stderr, "Inference error (line %d)\n", __LINE__); } } while(0)
+#define link_list(i) do { n->data[(i)].astPtr = n1; if (!ast_infer_node_type(n)) { fprintf(stderr, "Inference error (line %d)\n", __LINE__); } } while(0)
+
+    // func factorial(n int) (int) {
+    cf_make_function("factorial");
+    cf_assign_function_symtable(factorial);
+    cf_add_argument("n", CF_INT);
+    cf_add_return_value(NULL, CF_INT);
+
+    // dec_n := n - 1
+    cf_make_next_statement(CF_BASIC);
+    ASTNode *n1 = ast_leaf_id(sym_factorial_n);
+    ASTNode *n2 = ast_leaf_consti(1);
+    ASTNode *n = ast_node(AST_SUBTRACT);
+    link();
+
+    n1 = ast_leaf_id(sym_factorial_decN);
+    n2 = n;
+    n = ast_node(AST_DEFINE);
+    link();
+    cf_use_ast_explicit(n, CF_STATEMENT_BODY);
+
+    // if n < 2 {
+    cf_make_next_statement(CF_IF);
+    n1 = ast_leaf_id(sym_factorial_n);
+    n2 = ast_leaf_consti(2);
+    n = ast_node(AST_LOG_LT);
+    link();
+    cf_use_ast_explicit(n, CF_IF_CONDITIONAL);
+    // return 1
+    cf_make_if_then_statement(CF_RETURN);
+    cf_assign_statement_symtable(factorial_if_then);
+    n1 = ast_leaf_consti(1);
+    n = cf_ast_current();
+    link_list(0); // link 0 to predefined RETURN's AST_LIST
+    // } else {
+    cf_pop_previous_branched_statement();
+    cf_make_if_else_statement(CF_RETURN);
+    cf_assign_statement_symtable(factorial_if_else);
+    // return n * factorial(dec_n)
+
+    // -> funcall argument
+    n = ast_node_list(1);
+    n1 = ast_leaf_id(sym_factorial_decN);
+    link_list(0);
+
+    n2 = ast_node_func_call(sym_factorial, n);
+    n1 = ast_leaf_id(sym_factorial_n);
+    n = ast_node(AST_MULTIPLY);
+    link();
+    n1 = n;
+    n = cf_ast_current();
+    link_list(0); // link MULTIPLY to predefined RETURN's AST_LIST
+
+    // func main() {
+    cf_make_function("main");
+    cf_assign_function_symtable(main);
+    cf_make_next_statement(CF_BASIC);
+
+    // print("...")
+    n1 = ast_leaf_consts("Zadejte cislo pro vypocet faktorialu: ");
+    n = ast_node_list(1);
+    link_list(0);
+    n1 = n;
+    n = ast_node_func_call(sym_print, n1);
+    cf_use_ast_explicit(n, CF_STATEMENT_BODY);
+
+    // a, err := inputi()
+    cf_make_next_statement(CF_BASIC);
+    n1 = ast_node_list(0);
+    n = ast_node_func_call(sym_inputi, n1);
+
+    n2 = n;
+    n = ast_node_list(2);
+
+    // a, err AST_LIST
+    n1 = ast_leaf_id(sym_main_a);
+    link_list(0);
+    n1 = ast_leaf_id(sym_main_err);
+    link_list(1);
+
+    n1 = n;
+    n = ast_node(AST_DEFINE);
+    link();
+    cf_use_ast_explicit(n, CF_STATEMENT_BODY);
+
+    // if err == 0
+    cf_make_next_statement(CF_IF);
+    n2 = ast_leaf_consti(0);
+    n1 = ast_leaf_id(sym_main_err);
+    n = ast_node(AST_LOG_EQ);
+    link();
+    cf_use_ast_explicit(n, CF_IF_CONDITIONAL);
+    // {
+    //   if a < 0
+    cf_make_if_then_statement(CF_IF);
+    cf_assign_statement_symtable(main_if_then);
+    n2 = ast_leaf_consti(0);
+    n1 = ast_leaf_id(sym_main_a);
+    n = ast_node(AST_LOG_LT);
+    link();
+    cf_use_ast_explicit(n, CF_IF_CONDITIONAL);
+    //   {
+    cf_make_if_then_statement(CF_BASIC);
+    cf_assign_statement_symtable(main_if_then_if_then);
+    //      print("...", "\n")
+    n = ast_node_list(2);
+    n1 = ast_leaf_consts("Faktorial nejde spocitat!");
+    link_list(0);
+    n1 = ast_leaf_consts("\n");
+    link_list(1);
+    n1 = n;
+    n = ast_node_func_call(sym_print, n1);
+    cf_use_ast_explicit(n, CF_STATEMENT_BODY);
+    //   } else {
+    cf_pop_previous_branched_statement();
+    cf_make_if_else_statement(CF_BASIC);
+    cf_assign_statement_symtable(main_if_then_if_else);
+    //      vysl := factorial(a)
+
+    // -> funcall argument
+    n = ast_node_list(1);
+    n1 = ast_leaf_id(sym_main_a);
+    link_list(0);
+
+    n2 = ast_node_func_call(sym_factorial, n);
+    n1 = ast_leaf_id(sym_main_if_then_if_else_vysl);
+    n = ast_node(AST_DEFINE);
+    link();
+    cf_use_ast_explicit(n, CF_STATEMENT_BODY);
+
+    //     print("...", vysl, "\n")
+    cf_make_next_statement(CF_BASIC);
+    n = ast_node_list(3);
+    n1 = ast_leaf_consts("Vysledek je ");
+    link_list(0);
+    n1 = ast_leaf_id(sym_main_if_then_if_else_vysl);
+    link_list(1);
+    n1 = ast_leaf_consts("\n");
+    link_list(2);
+    n1 = n;
+    n = ast_node_func_call(sym_print, n1);
+    cf_use_ast_explicit(n, CF_STATEMENT_BODY);
+
+    // } else {
+    cf_pop_previous_branched_statement();
+    cf_pop_previous_branched_statement();
+    cf_make_if_else_statement(CF_BASIC);
+    cf_assign_statement_symtable(main_if_else);
+    n1 = ast_leaf_consts("Chyba pri nacitani celeho cisla!\n");
+    n = ast_node_list(1);
+    link_list(0);
+    n1 = n;
+    n = ast_node_func_call(sym_print, n1);
+    cf_use_ast_explicit(n, CF_STATEMENT_BODY);
+}
+
 int main() {
-    test2();
+    factorial_rec();
     tcg_generate();
 
     return compiler_result;
